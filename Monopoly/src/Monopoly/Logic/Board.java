4be92391cac35672da05117d17f6c5c7dc1a0be6
agfac <andregfcorreia@gmail.com>
2016-05-29 17:59:44 +0100

    Some board box inserted

--- Monopoly/src/Monopoly/Logic/Board.java
+++ Monopoly/src/Monopoly/Logic/Board.java
@@ -3,31 +3,29 @@ package Monopoly.Logic;
 import java.util.*;
 
 /**
- * QfcWAvgW
+ * Board Class
  */
 public class Board {
-
+	
+	public Vector<Player> players;
+    public Vector<Deck> deck;
+    public BoardBox[] boxs;
+    
     /**
-     * Default constructor
+     * Board Constructor
      */
     public Board() {
+    	
+    	GoBox Go = new GoBox();
+    	NormalProperty MediterraneanAvenue = new NormalProperty(1,"MediterraneanAvenue", 1, 60, 30, 2, new int[]{10,30,90,160}, 250);
+    	CommunityBox CommunityBox1 = new CommunityBox(2);
+    	NormalProperty BalticAvenue = new NormalProperty(3,"BalticAvenue", 1, 60, 30, 4, new int[]{20,60,180,320}, 450);
+    	TaxBox IncomeTax = new TaxBox(4,"Income Tax",200);
+    	RailRoadProperty ReadingRailRoad = new RailRoadProperty(5, "ReadingRailRoad");
+    	NormalProperty OrientalAvenue = new NormalProperty(6, "OrientalAvenue", 3, 100, 50, 6, new int[]{30,90,270,400}, 550);
+    	NormalProperty VermontAvenue = new NormalProperty(7, "VermontAvenue", 3, 100, 50, 6, new int[]{30,90,270,400}, 550);
+    	NormalProperty ConnecticutAvenue = new NormalProperty(8, "ConnecticutAvenue", 3, 120, 60, 8, new int[]{40,100,300,450}, 600);
     }
-
-    /**
-     * 
-     */
-    public Vector<Player> players;
-
-    /**
-     * 
-     */
-    public Vector<Deck> deck;
-
-    /**
-     * 
-     */
-    public BoardBox[] boxs;
-
 
     /**
      * @param players
