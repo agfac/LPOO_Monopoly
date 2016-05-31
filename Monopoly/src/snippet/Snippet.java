9047f45951831811b1c6eafb544c565dc207cca4
Fabiola <ffabiolasilva@gmail.com>
2016-05-31 21:28:07 +0100

    FARTA DESTA MERDA

--- Monopoly/src/snippet/Snippet.java
+++
@@ -1,9 +0,0 @@
-package snippet;
-
-public class Snippet {
-	<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
-	<uses-permission android:name="android.permission.INTERNET" />
-	<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
-	<uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
-}
-
