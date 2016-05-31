8746b4311ecf46f4f98b7f0912726c9a5084f313
Fabiola <ffabiolasilva@gmail.com>
2016-05-31 21:18:10 +0100

    aSQ

---
+++ Monopoly/src/snippet/Snippet.java
@@ -0,0 +1,9 @@
+package snippet;
+
+public class Snippet {
+	<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+	<uses-permission android:name="android.permission.INTERNET" />
+	<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+	<uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+}
+
