94ee44ccf191e51b093fed6fcac10319f98c6a7a
agfac <andregfcorreia@gmail.com>
2016-06-01 22:01:40 +0100

    Server already receive data from client!
    Create player with success!

--- Monopoly/src/Monopoly/Server/Server.java
+++ Monopoly/src/Monopoly/Server/Server.java
@@ -1,41 +1,65 @@
 package Monopoly.Server;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import Monopoly.Logic.Player;
 
 public class Server {
 
 	private static ServerSocket serverSocket;
 	private static Socket clientSocket;
 	private static InputStreamReader inputStreamReader;
+	private static OutputStream ouputStream;
+	private static BufferedOutputStream bufferedOutputStream;
+	private static ObjectOutputStream objectOutputStream;
 	private static BufferedReader bufferedReader;
 	private static String message;
 
 	public static void main(String[] args) {
 		
 		try {
-			serverSocket = new ServerSocket(80); // Server socket
+			serverSocket = new ServerSocket(4444); // Server socket
 	
 		} catch (IOException e) {
-			System.out.println("Could not listen on port: 80");
+			System.out.println("Could not listen on port: 4444");
 		}
 	
-		System.out.println("Server started. Listening to the port 80");
+		System.out.println("Server started. Listening to the port 4444");
 	
 		while (true) {
 			try {
 	
 				clientSocket = serverSocket.accept(); // accept the client connection
+				
+				/*ouputStream = clientSocket.getOutputStream();
+				Player p = new Player("Andre");
+				SerializationUtil.serialize(p, ouputStream);*/
+				
 				inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
 				bufferedReader = new BufferedReader(inputStreamReader); // get the client message
 				message = bufferedReader.readLine();
 	
-				System.out.println(message);
+				String[] parts = message.split(";");
+				String name = parts[0];
+				int pieceId = Integer.parseInt(parts[1]);
+
+				
+				Player p = new Player(name, pieceId);
+				System.out.println(p.getName());
+				System.out.println(p.getPieceId());
+				//System.out.println(message);
 				inputStreamReader.close();
 				clientSocket.close();
+				//ouputStream.close();
+				clientSocket.close();
+				
 	
 			} catch (IOException ex) {
 				System.out.println("Problem in message reading");
