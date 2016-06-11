3e4a28fff2ff756ea495b1555cf61e3d963eb3ef
agfac <andregfcorreia@gmail.com>
2016-06-11 23:08:10 +0100

    GameProtocol updated

--- Monopoly/src/Monopoly/Server/GameProtocol.java
+++ Monopoly/src/Monopoly/Server/GameProtocol.java
@@ -247,6 +247,22 @@ public class GameProtocol {
 				game.updateGame(currentPlayer);
 				state = PLAYING;
 				break;
+				
+			case "Manage activity":
+				theOutput="Properties:";
+				for(Property p:currentPlayer.getPropertiesOwned()){
+					if(!(p.equals(currentPlayer.getPropertiesOwned().lastElement()))){
+						theOutput+=p.getName();
+						theOutput+=";";
+					}
+					else{
+						theOutput=p.getName();
+						theOutput+=".";
+					}
+						
+				}
+				state = MANAGEACTIVITY;
+				break;
 		}
 			
 			break;
@@ -440,9 +456,7 @@ public class GameProtocol {
 				}
 				}
 				break;
-			case "Manage activity":
-				state = MANAGEACTIVITY;
-				break;
+			
 			}
 			break;
 		case PAYBILL:
